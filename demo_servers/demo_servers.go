256a7b024fc042b356717d6339d0a042194a8383
mdolin <mihadlnr@gmail.com>
2025-10-05 14:56:16 +0200

    Add demo servers

---
+++ demo_servers/demo_servers.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func startServer(port int) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/api/v1", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(404)
+		res := fmt.Sprintf("Hello From Server: %d", port)
+		io.Copy(w, strings.NewReader(res))
+	})
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	})
+
+	mux.HandleFunc("/*", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(404)
+		io.Copy(w, strings.NewReader("Not Found"))
+	})
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: mux,
+	}
+
+	log.Printf("Starting server on port %d", port)
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Server on port %d failed: %v", port, err)
+	}
+}
+
+func main() {
+	go startServer(3001)
+	go startServer(3002)
+	go startServer(3003)
+
+	select {}
+}
